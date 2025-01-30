b8ddc62c6d48344d2e1c52c588a9ede6c04b0777
theo <ishesensei@gmail.com>
2025-01-30 15:42:48 +0530

    implement use client in page!

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,37 +1,7 @@
+'use client';
+import { createPost } from '@/actions/posts';
 import PostForm from '@/components/post-form';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
 
-export default async function NewPostPage() {
-  async function createPost(prevFormState, formData) {
-    //server action
-    'use server';
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push('Title is required.');
-    }
-    if (!content || content.trim().length === 0) {
-      errors.push('Content is required.');
-    }
-    if (!image || image.size === 0) {
-      errors.push('image is required.');
-    }
-    if (errors.length > 0) {
-      return { errors };
-    }
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect('/feed');
-  }
+export default function NewPostPage() {
   return <PostForm action={createPost} />;
 }
